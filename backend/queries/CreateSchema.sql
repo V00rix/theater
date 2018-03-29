8054f772fd2525961ed0625c76ec977e3fcc139e
v00rix <vladogim97@gmail.com>
2018-03-29 10:55:21 +0200

    Test data SQL

--- backend/queries/CreateSchema.sql
+++ backend/queries/CreateSchema.sql
@@ -3,25 +3,25 @@ CREATE DATABASE IF NOT EXISTS Theater;
 USE Theater;
 
 /* Drop tables */
-DROP TABLE IF EXISTS T_Review;
-DROP TABLE IF EXISTS T_Friendship;
-DROP TABLE IF EXISTS T_Profile;
-DROP TABLE IF EXISTS T_Ticket;
-DROP TABLE IF EXISTS T_Order;
-DROP TABLE IF EXISTS T_Seat;
-DROP TABLE IF EXISTS T_Session;
-DROP TABLE IF EXISTS T_Chair;
-DROP TABLE IF EXISTS T_Hall;
-DROP TABLE IF EXISTS T_Theater;
-DROP TABLE IF EXISTS T_Phone_Client;
-DROP TABLE IF EXISTS T_Website_Client;
-DROP TABLE IF EXISTS T_Registered_User;
-DROP TABLE IF EXISTS T_Timestamp;
-DROP TABLE IF EXISTS T_Performance;
-DROP TABLE IF EXISTS T_Address;
+DROP TABLE IF EXISTS t_review;
+DROP TABLE IF EXISTS t_friendship;
+DROP TABLE IF EXISTS t_profile;
+DROP TABLE IF EXISTS t_ticket;
+DROP TABLE IF EXISTS t_order;
+DROP TABLE IF EXISTS t_seat;
+DROP TABLE IF EXISTS t_session;
+DROP TABLE IF EXISTS t_chair;
+DROP TABLE IF EXISTS t_hall;
+DROP TABLE IF EXISTS t_theater;
+DROP TABLE IF EXISTS t_phone_client;
+DROP TABLE IF EXISTS t_website_client;
+DROP TABLE IF EXISTS t_registered_user;
+DROP TABLE IF EXISTS t_timestamp;
+DROP TABLE IF EXISTS t_performance;
+DROP TABLE IF EXISTS t_address;
 
 /* Create Tables */
-CREATE TABLE T_Address (
+CREATE TABLE t_address (
   id        INT AUTO_INCREMENT UNIQUE NOT NULL,
   country   VARCHAR(255) CHARACTER SET utf8
   COLLATE utf8_general_ci             NOT NULL,
@@ -36,7 +36,7 @@ CREATE TABLE T_Address (
   PRIMARY KEY (country, city, street, house)
 );
 
-CREATE TABLE T_Performance (
+CREATE TABLE t_performance (
   id          INT AUTO_INCREMENT UNIQUE NOT NULL,
   author      VARCHAR(255),
   title       VARCHAR(255) UNIQUE,
@@ -45,126 +45,143 @@ CREATE TABLE T_Performance (
   PRIMARY KEY (author, title)
 );
 
-CREATE TABLE T_Timestamp (
+CREATE TABLE t_timestamp (
   date TIMESTAMP PRIMARY KEY
 );
 
-CREATE TABLE T_Registered_User (
+CREATE TABLE t_registered_user (
   email    VARCHAR(255) PRIMARY KEY,
   login    VARCHAR(255) NOT NULL UNIQUE,
   name     VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE T_Website_Client (
+CREATE TABLE t_website_client (
   email VARCHAR(255) PRIMARY KEY,
   name  VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE T_Phone_Client (
+CREATE TABLE t_phone_client (
   phone VARCHAR(255) PRIMARY KEY,
   name  VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE T_Theater (
+CREATE TABLE t_theater (
   id      INT AUTO_INCREMENT UNIQUE NOT NULL,
   address INT                       NOT NULL UNIQUE,
   name    VARCHAR(255)              NOT NULL UNIQUE,
   PRIMARY KEY (address, name),
-  FOREIGN KEY (address) REFERENCES T_Address (id)
+  FOREIGN KEY (address) REFERENCES t_address (id)
     ON DELETE CASCADE
 );
 
-CREATE TABLE T_Hall (
+CREATE TABLE t_hall (
   id      INT AUTO_INCREMENT UNIQUE NOT NULL,
   name    VARCHAR(255),
   theater INT,
-  FOREIGN KEY (theater) REFERENCES T_Theater (id)
+  FOREIGN KEY (theater) REFERENCES t_theater (id)
     ON DELETE CASCADE,
   PRIMARY KEY (name, theater)
 );
 
 
-CREATE TABLE T_Chair (
+CREATE TABLE t_chair (
   id     INT AUTO_INCREMENT UNIQUE NOT NULL,
   row    INT,
   number INT,
   hall   INT,
   PRIMARY KEY (row, number),
-  FOREIGN KEY (hall) REFERENCES T_Hall (id)
+  FOREIGN KEY (hall) REFERENCES t_hall (id)
     ON DELETE CASCADE
 );
 
-CREATE TABLE T_Session (
+CREATE TABLE t_session (
   id           INT AUTO_INCREMENT UNIQUE NOT NULL,
   date         TIMESTAMP,
   hall_name    VARCHAR(255),
   hall_theater INT,
   performance  INT,
   PRIMARY KEY (date, hall_name, hall_theater, performance),
-  FOREIGN KEY (hall_name, hall_theater) REFERENCES T_Hall (name, theater)
+  FOREIGN KEY (hall_name, hall_theater) REFERENCES t_hall (name, theater)
     ON DELETE CASCADE,
-  FOREIGN KEY (performance) REFERENCES T_Performance (id),
-  FOREIGN KEY (date) REFERENCES T_Timestamp (date)
-);
-
-CREATE TABLE T_Seat (
-  chair   INT,
-  session INT,
-  availabillity ENUM('FREE', 'BOOKED', 'HIDDEN'),
-  PRIMARY KEY (chair, session),
-  FOREIGN KEY (chair) REFERENCES T_Chair (id)
+  FOREIGN KEY (performance) REFERENCES t_performance (id)
     ON DELETE CASCADE,
-  FOREIGN KEY (session) REFERENCES T_Session (id)
+  FOREIGN KEY (date) REFERENCES t_timestamp (date)
     ON DELETE CASCADE
 );
 
-CREATE TABLE T_Order (
-  id               INT AUTO_INCREMENT UNIQUE NOT NULL,
+CREATE TABLE t_seat (
+  chair         INT,
+  session       INT,
+  availabillity ENUM ('FREE', 'BOOKED', 'HIDDEN') NOT NULL DEFAULT 'FREE',
+  PRIMARY KEY (chair, session),
+  FOREIGN KEY (chair) REFERENCES t_chair (id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (session) REFERENCES t_session (id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE t_order (
+  id               INT AUTO_INCREMENT UNIQUE                                   NOT NULL,
   date             TIMESTAMP PRIMARY KEY,
   registered_email VARCHAR(255),
   website_email    VARCHAR(255),
   phone_number     VARCHAR(255),
-  type             ENUM ('RESERVATION', 'PURCHASE'),
-  checkout         ENUM ('DELIVERY', 'SELF_CHECKOUT', 'PAY_BEFORE'),
-  FOREIGN KEY (date) REFERENCES T_Timestamp (date),
-  FOREIGN KEY (registered_email) REFERENCES T_Registered_User (email),
-  FOREIGN KEY (website_email) REFERENCES T_Website_Client (email),
-  FOREIGN KEY (registered_email) REFERENCES T_Phone_Client (phone)
+  is_digital       BOOL DEFAULT TRUE                                           NOT NULL,
+  type             ENUM ('RESERVATION', 'PURCHASE')                            NOT NULL,
+  checkout         ENUM ('DELIVERY', 'SELF_CHECKOUT', 'PAY_BEFORE')            NOT NULL,
+  FOREIGN KEY (date) REFERENCES t_timestamp (date)
+    ON DELETE CASCADE,
+  FOREIGN KEY (registered_email) REFERENCES t_registered_user (email)
+    ON DELETE SET NULL,
+  FOREIGN KEY (website_email) REFERENCES t_website_client (email)
+    ON DELETE SET NULL,
+  FOREIGN KEY (phone_number) REFERENCES t_phone_client (phone)
+    ON DELETE SET NULL,
+  CHECK (registered_email IS NOT NULL OR website_email IS NOT NULL OR phone_number IS NOT NULL)
 );
 
-CREATE TABLE T_Ticket (
-  `order` INT,
+CREATE TABLE t_ticket (
+  `order` INT NOT NULL,
   chair   INT,
   session INT,
   PRIMARY KEY (chair, session),
-  FOREIGN KEY (chair, session) REFERENCES T_Seat (chair, session)
+  FOREIGN KEY (chair, session) REFERENCES t_seat (chair, session)
     ON DELETE CASCADE,
-  FOREIGN KEY (`order`) REFERENCES T_Order (id)
+  FOREIGN KEY (`order`) REFERENCES t_order (id)
+    ON DELETE CASCADE
 );
 
-CREATE TABLE T_Profile (
+CREATE TABLE t_profile (
   email    VARCHAR(255) PRIMARY KEY,
-  photo    BLOB,
+  photo    LONGBLOB,
   facebook VARCHAR(255),
   google   VARCHAR(255),
   twitter  VARCHAR(255),
-  FOREIGN KEY (email) REFERENCES T_Registered_User (email)
+  FOREIGN KEY (email) REFERENCES t_registered_user (email)
+    ON DELETE CASCADE
 );
 
-CREATE TABLE T_Friendship (
+CREATE TABLE t_friendship (
   email_1 VARCHAR(255),
   email_2 VARCHAR(255),
   PRIMARY KEY (email_1, email_2),
-  FOREIGN KEY (email_1) REFERENCES T_Registered_User (email),
-  FOREIGN KEY (email_2) REFERENCES T_Registered_User (email),
+  FOREIGN KEY (email_1) REFERENCES t_registered_user (email)
+    ON DELETE CASCADE,
+  FOREIGN KEY (email_2) REFERENCES t_registered_user (email)
+    ON DELETE CASCADE,
   CHECK (email_1 != email_2)
 );
 
-CREATE TABLE T_Review (
+CREATE TABLE t_review (
   date        TIMESTAMP,
   email       VARCHAR(255),
   performance INT,
   PRIMARY KEY (date, email, performance),
-  FOREIGN KEY (performance) REFERENCES T_Performance (id)
+  FOREIGN KEY (performance) REFERENCES t_performance (id)
+    ON DELETE CASCADE,
+  FOREIGN KEY (date) REFERENCES t_timestamp (date)
+    ON DELETE CASCADE,
+  FOREIGN KEY (email) REFERENCES t_registered_user(email)
+    ON DELETE CASCADE
 );
--- backend/queries/InsertTestData.sql
+++ backend/queries/InsertTestData.sql
@@ -1,32 +1,64 @@
 USE Theater;
+
 DELETE FROM t_address;
-
-
 INSERT INTO t_address (id, country, city, street, house, post_code, city_part) VALUES
   (10, 'Ukraine', 'Kiev', 'Amosova st.', '4', '141 00', NULL),
   (1, 'Czech republic', 'Prague', 'Na Zátorce', '1048/16', '160 00', 'Praha 6 Bubeneč');
-
+SELECT *
+FROM t_address;
 
 DELETE FROM t_performance;
-INSERT INTO t_performance (id, author, title, description, actors) VALUES
-  (1, 'Great Author', 'My spectacular creation', NULL, NULL);
-
+INSERT INTO t_performance (id, author, title) VALUES
+  (1, 'Great Author', 'My spectacular creation');
+SELECT *
+FROM t_performance;
 
 DELETE FROM t_timestamp;
 INSERT INTO t_timestamp (date) VALUES
-  ('2018-04-13 19:00:00'), ('2018-04-13 21:00:00'), ('2018-04-20 19:00:00');
+  ('2018-04-13 19:00:00'), ('2018-04-13 21:00:00'), ('2018-04-20 19:00:00'),
+  ('2018-03-29 10:00:00'), ('2018-03-29 11:00:00');
+SELECT *
+FROM t_timestamp;
 
+DELETE FROM t_website_client;
+INSERT INTO t_website_client (email, name) VALUES
+  ('vladogim97@gmail.com', 'Vladik'),
+  ('pidorpidor@pidor.pidor', 'Pavel Kuznetsov');
+SELECT *
+FROM t_website_client;
+
+DELETE FROM t_registered_user;
+INSERT INTO t_registered_user (email, login, name, password) SELECT
+                                                               email,
+                                                               'v00rix',
+                                                               name,
+                                                               'password'
+                                                             FROM t_website_client
+                                                             WHERE email = 'vladogim97@gmail.com';
+INSERT INTO t_registered_user (email, login, name, password) VALUES
+  ('my@mail.com', 'JUST_A_FRIEND', 'hello', 'heslo');
+SELECT *
+FROM t_registered_user;
+
+DELETE FROM t_phone_client;
+INSERT INTO t_phone_client (phone, name) VALUES
+  ('123123123', 'TEST_PHONE_CLIENT'),
+  ('776558334', 'Vladik');
+SELECT *
+FROM t_phone_client;
 
 DELETE FROM t_theater;
-INSERT INTO t_theater (id, address, name) VALUES
+INSERT INTO t_theater (id, address, name) VALUE
   (1, 1, 'TEST_THEATER'),
   (10, 10, 'WRONG_THEATER');
-
+SELECT *
+FROM t_theater;
 
 DELETE FROM t_hall;
 INSERT INTO t_hall (id, name, theater) VALUES
   (1, 'TEST_HALL', 1);
-
+SELECT *
+FROM t_hall;
 
 DELETE FROM t_chair;
 DROP PROCEDURE IF EXISTS createSeats;
@@ -63,7 +95,8 @@ DELIMITER ;
 
 CALL createRows();
 # INSERT INTO T_Chair...
-
+SELECT *
+FROM t_chair;
 
 DELETE FROM t_session;
 INSERT INTO t_session (date, hall_name, hall_theater, performance) SELECT
@@ -78,6 +111,9 @@ INSERT INTO t_session (date, hall_name, hall_theater, performance) SELECT
                                                                      STRAIGHT_JOIN t_timestamp tsp
                                                                      STRAIGHT_JOIN t_performance p
                                                                    ORDER BY tsp.date;
+SELECT *
+FROM t_session;
+
 DELETE FROM t_seat;
 INSERT INTO t_seat (chair, session) SELECT
                                       c.id,
@@ -85,10 +121,43 @@ INSERT INTO t_seat (chair, session) SELECT
                                     FROM t_session s
                                       STRAIGHT_JOIN t_chair c
                                     ORDER BY s.id;
-SELECT * FROM t_seat;
+SELECT *
+FROM t_seat;
 
-#
-# SELECT *
-# FROM t_theater
-#   JOIN t_address a ON t_theater.address = a.id
-#   JOIN t_hall h ON t_theater.id = h.theater;
+SELECT *
+FROM t_order;
+DELETE FROM t_order;
+INSERT INTO t_order (id, date, registered_email, website_email, type, checkout) VALUE
+  (1, '2018-03-29 10:00:00', 'vladogim97@gmail.com', 'vladogim97@gmail.com', 'RESERVATION', 'SELF_CHECKOUT');
+SELECT *
+FROM t_order;
+
+DELETE FROM t_ticket;
+INSERT INTO t_ticket (`order`, chair, session) SELECT
+                                                 1,
+                                                 c.id,
+                                                 s.id
+                                               FROM t_chair c
+                                                 JOIN t_seat seat ON c.id = seat.chair
+                                                 JOIN t_session s ON seat.session = s.id
+                                               WHERE row = 5 AND number = 14 AND s.date = '2018-04-13 19:00:00';
+SELECT *
+FROM t_ticket;
+
+DELETE FROM t_profile;
+INSERT INTO t_profile (email, photo, facebook) VALUES
+  ('vladogim97@gmail.com', LOAD_FILE('C:\\Users\\User\\IdeaProjects\\theater\\backend\\queries\\assets\\test.png'), 'https://www.facebook.com/v.yazykov');
+SELECT *
+FROM t_profile;
+
+DELETE FROM t_friendship;
+INSERT INTO t_friendship (email_1, email_2) VALUES
+  ('my@mail.com', 'vladogim97@gmail.com');
+SELECT *
+FROM t_friendship;
+
+DELETE FROM t_review;
+INSERT INTO t_review (date, email, performance) VALUES
+  ('2018-03-29 11:00:00', 'vladogim97@gmail.com', 1);
+SELECT *
+FROM t_review;
